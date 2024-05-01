9f973d59c8f5be1461068fbe4e7b66765f8bdcd4
Ayush kumar <ayushkumar@adsl-ull-59-0.51-151.wind.it>
2024-05-01 22:02:34 +0530

    filesadded

---
+++ myapp/src/Comp/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+
+
+
+const Layout = ({ children}) => {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "100vh" }}>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+
+
+export default Layout;
---
+++ myapp/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import React from "react";
+// import './styles/Home.css'
+import Layout from "../Comp/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Home;
